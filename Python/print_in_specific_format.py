7e08f37300def204fd322e3d44dca96c29c1f318
Mayank Khanna <khanna98@outlook.com>
2018-10-24 00:28:16 +0530

    Added comments

--- Python/print_in_specific_format.py
+++ Python/print_in_specific_format.py
@@ -1,8 +1,8 @@
-# Write a Python program to print the following string in a specific format (see the output). Go to the editor
+# Q. Write a Python program to print the following string in a specific format (see the output). Go to the editor
 # Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky.
 #  Twinkle, twinkle, little star, How I wonder what you are" 
 # Output :
-
+#
 # Twinkle, twinkle, little star,
 # 	 How I wonder what you are! 
 # 		 Up above the world so high,   		
@@ -11,8 +11,8 @@
 # 	 How I wonder what you are
 
 print("Twinkle, twinkle, little star,")
-print("\t How I wonder what you are!")
-print("\t\t Up above the world so high,")
-print("\t\t Like a diamond in the sky.")
+print("\t How I wonder what you are!") # Using escape sequences to match the format
+print("\t\t Up above the world so high,") # Not using \n as it is a by default for print in Python
+print("\t\t Like a diamond in the sky.") # \t stands for tabspace 
 print("Twinkle, twinkle, little star,")
 print("\t How I wonder what you are!")
