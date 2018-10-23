e0b4d7181ea1348fd629e64e103a165f026d8af2
Mayank Khanna <khanna98@outlook.com>
2018-10-24 00:11:47 +0530

    Print in specific format

---
+++ Python/print_in_specific_format.py
@@ -0,0 +1,18 @@
+# Write a Python program to print the following string in a specific format (see the output). Go to the editor
+# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky.
+#  Twinkle, twinkle, little star, How I wonder what you are" 
+# Output :
+
+# Twinkle, twinkle, little star,
+# 	 How I wonder what you are! 
+# 		 Up above the world so high,   		
+# 		 Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	 How I wonder what you are
+
+print("Twinkle, twinkle, little star,")
+print("\t How I wonder what you are!")
+print("\t\t Up above the world so high,")
+print("\t\t Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("\t How I wonder what you are!")
