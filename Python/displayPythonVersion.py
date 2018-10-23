af708ba95559c4346609428ebe40c8d0bb21a44c
Mayank Khanna <khanna98@outlook.com>
2018-10-24 00:06:41 +0530

    Display Python Version

---
+++ Python/displayPythonVersion.py
@@ -0,0 +1,7 @@
+# For displaying the Python version we first need to import sys module
+import sys
+
+print("Python version : ",end=" ")
+print(sys.version)
+print("Version info : ",end=" ")
+print(sys.version_info)
