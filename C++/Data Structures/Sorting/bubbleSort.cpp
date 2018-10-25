be39998b6e0290fc35846b3644959beb608febb4
Mayank Khanna <khanna98@outlook.com>
2018-10-26 01:23:54 +0530

    Bubble Sort

--- C++/Data Structures/Sorting/bubbleSort.cpp
+++ C++/Data Structures/Sorting/bubbleSort.cpp
@@ -7,14 +7,14 @@ void swap(int *m,int *n){
     *n = temp;
 }
 
-void printArray(int *arr,int n){
+void printArray(int arr[],int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
 
-void bubbleSort(int *arr,int n){
+void bubbleSort(int arr[],int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j] > arr[j+1]){
@@ -27,10 +27,11 @@ void bubbleSort(int *arr,int n){
 int main(void){
     int arr[] = {20,5,78,9,2,3,56,19};
     int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<n<<endl;
+    //cout<<n<<endl;
     cout<<"The unsorted array is : ";
     printArray(arr,n);
     bubbleSort(arr,n);
     cout<<"After sorting : ";
     printArray(arr,n);
+    return 0;
 }
