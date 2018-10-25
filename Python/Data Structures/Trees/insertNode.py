ce7557b43a427ea4bbb9c9b414d37ee3eeb11d29
Mayank Khanna <khanna98@outlook.com>
2018-10-25 23:57:00 +0530

    Insert Node

---
+++ Python/Data Structures/Trees/insertNode.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+    
+    def insert(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data, end = " ")
+        if self.right:
+            self.right.printTree()
+
+
+if __name__ == '__main__':
+    root = Node(10)
+    root.insert(12)
+    root.insert(6)
+    root.insert(14)
+    root.insert(3)
+    root.insert(4)
+
+    root.printTree()
+    print()
