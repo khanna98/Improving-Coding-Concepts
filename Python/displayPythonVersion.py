fc8d799856421c0cdb10055d9190c9bc6e5f3e3b
Mayank Khanna <khanna98@outlook.com>
2018-11-11 19:24:02 +0530

    Area of circle

--- Python/displayPythonVersion.py
+++
@@ -1,9 +0,0 @@
-# Q. Write a Python program to get the Python version you are using.
-
-# For displaying the Python version we first need to import sys module
-import sys
-
-print("Python version : ",end=" ")
-print(sys.version) # Using the version function 
-print("Version info : ",end=" ")
-print(sys.version_info) # Using the version_info function
