fc5c56c172dab7ed2ce074ce5c5b2126bc108a08
Mayank Khanna <khanna98@outlook.com>
2018-11-20 22:44:43 +0530

    Heap Sort

--- C++/Data Structures/Sorting/heapSort.cpp
+++ C++/Data Structures/Sorting/heapSort.cpp
@@ -1,41 +1,32 @@
 #include<iostream>
 using namespace std;
 
+void heapify(int arr[],int n,int i){
+    int largest,l,r;
+    largest = i;
+    l = 2*i+1;
+    r = 2*i+2;
+
+    if(l<n && arr[l]>arr[largest]){
+        largest = l;
+    }
+
+    if(r<n && arr[r]>arr[largest]){
+        largest = r;
+    }
+
+    if(largest!=i){
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }
+}
+
 void printArray(int arr[],int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
-
-void swap(int *a,int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void heapify(int arr[],int n,int i){
-    int largest = i; // Initialize largest as root
-    int l = 2*i + 1; // Left = 2*i+1
-    int r = 2*i + 2; // Right = 2*i+2
-
-    // If left child is larger than the root
-    if(l<n && arr[l] >arr[largest]){
-        largest = l;
-    }
-
-    // If right child is greater than the Largest so far
-    if(r<n && arr[r] > arr[largest]){
-        largest = r;
-    }
-
-    // If largest is not root
-    if(largest != i){
-        swap(&arr[i],&arr[largest]);
-        heapify(arr,n,largest);
-    }
-}
-
 void heapSort(int arr[],int n){
     for(int i=n/2-1;i>=0;i--){
         heapify(arr,n,i);
@@ -43,18 +34,15 @@ void heapSort(int arr[],int n){
 
     for(int i=n-1;i>=0;i--){
         swap(arr[0],arr[i]);
-
         heapify(arr,i,0);
     }
 }
 
 int main(void){
-    int arr[] = {12,11,1,8,3,2,20};
+    int arr[] = {12,11,5,6,1,2,4};
     int n = sizeof(arr)/sizeof(arr[0]);
 
     heapSort(arr,n);
-
     cout<<"Sorted Array : ";
     printArray(arr,n);
-    return 0;
 }
