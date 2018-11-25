7565d2791ad137608d940c4cac43b06ebd245c22
Mayank Khanna <khanna98@outlook.com>
2018-11-25 23:43:03 +0530

    Bubble Sort

--- Java/Data Structures/Sorting/bubbleSort.java
+++ Java/Data Structures/Sorting/bubbleSort.java
@@ -21,6 +21,7 @@ class Test{
         for(int i=0;i<n;i++){
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
