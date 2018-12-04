1d936e8871062d328adfc4650feee6b100e7f603
Mayank Khanna <khanna98@outlook.com>
2018-12-04 23:22:32 +0530

    Update array1.py

--- Python/Data Structures/Arrays/array1.py
+++ Python/Data Structures/Arrays/array1.py
@@ -3,7 +3,8 @@
 # arrayName = array(typecode, [Initializers])
 
 from array import *
-array1 = array('i',[10,20,30,40,50]) # Here 'i' stands for Signed integer of size 2 bytes
+
+array1 = array('i',[10,20,30,40,50,60]) # Here 'i' stands for Signed integer of size 2 bytes
 
 for x in array1:
 	print(x)
