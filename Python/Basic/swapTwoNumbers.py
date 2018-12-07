66d9c1fea7a34d06aeb81bbf82f99ec50c5ee25d
Mayank Khanna <khanna98@outlook.com>
2018-12-07 23:04:03 +0530

    Update swapTwoNumbers.py

--- Python/Basic/swapTwoNumbers.py
+++ Python/Basic/swapTwoNumbers.py
@@ -1,7 +1,8 @@
+# Input two numbers
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-
 print("Before swap : ",a,b)
 
+# Swapping two numbers
 a,b = b,a
 print("After swap : ",a,b)
