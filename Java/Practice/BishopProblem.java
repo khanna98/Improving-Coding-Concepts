62d5915c3ff3b2f1d5427d6c8faaf46a00189eed
Mayank Khanna <khanna98@outlook.com>
2018-12-10 23:31:47 +0530

    Bishop Problem

---
+++ Java/Practice/BishopProblem.java
@@ -0,0 +1,14 @@
+
+// This is the solution to the Bishop Problem
+// i.e the maximum number of bishops that can be
+// placed on a NxN chessboard that don't collide
+
+import java.util.*;
+
+class BishopProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Size of the board
+        System.out.println("Maximum bishops that can be placed are : " + 2 * (n - 1));
+    }
+}
