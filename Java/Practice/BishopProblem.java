8d6dd3f5bca38e068a442c1fa1560a6b2aa916fa
Mayank Khanna <khanna98@outlook.com>
2018-12-11 23:37:37 +0530

    Update BishopProblem.java

--- Java/Practice/BishopProblem.java
+++ Java/Practice/BishopProblem.java
@@ -7,8 +7,8 @@ import java.util.*;
 
 class BishopProblem {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Create Scanner object to input number
         int n = sc.nextInt(); // Size of the board
-        System.out.println("Maximum bishops that can be placed are : " + 2 * (n - 1));
+        System.out.println("Maximum bishops that can be placed are : " + 2 * (n - 1)); // Answer using the formula
     }
 }
