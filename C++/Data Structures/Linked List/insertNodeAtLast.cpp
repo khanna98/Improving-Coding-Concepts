47eb7d8c1f63c36decf3fbb1887b464cb9d0dd3b
Mayank Khanna <khanna98@outlook.com>
2018-12-15 23:40:19 +0530

    Insert At Last - Linked List

---
+++ C++/Data Structures/Linked List/insertNodeAtLast.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    struct node *next;
+}*head;
+
+class LinkedList{
+public:
+    void insertAtLast(struct node **ptr,int num){
+        struct node *n = new node;
+        n->data = num;
+        n->next = NULL;
+        struct node *cur = (*ptr);
+        if(*ptr == NULL){
+            *ptr = n;
+            return;
+        }
+        while(cur->next!=NULL){
+            cur=cur->next;
+        }
+        cur->next = n;
+        return;
+    }
+
+    void printList(struct node *ptr){
+        for(; ptr != NULL; ptr = ptr->next){
+            cout << ptr->data << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main(void){
+    LinkedList ll;
+    head = NULL;
+    int n, a;
+    cout << "Enter the size of list : ";
+    cin >> n;
+    cout << "Enter the elements of the list : " << endl;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        ll.insertAtLast(&head, a);
+    }
+    ll.printList(head);
+}
